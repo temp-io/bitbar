6a07ac2831d17c982bf86ee982fa5e36048d7971
Ten Wong <wangtengoo7@gmail.com>
2017-01-06 18:05:47 +0800

    support F

--- temp-io.5m.py
+++ temp-io.5m.py
@@ -22,7 +22,7 @@ temp_id = '50f7fa7ab532a9538fd1386d17b016c4'
 token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjI0MTE2NWVlYTAzOGFmYmZjOWE4Mjc3YmUzODJhNjQ4MTY3YzAyMGEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vd2lvYXBwLWM3MGE4IiwibmFtZSI6IldvbmcgVGVuIiwicGljdHVyZSI6Imh0dHBzOi8vc2NvbnRlbnQueHguZmJjZG4ubmV0L3YvdDEuMC0xL3MxMDB4MTAwLzExMjIyNDYzXzEwNjg0MTIxMzAxMzA4OF8xMDgxNzE3OTQxOTcwNzM3NDk1X24uanBnP29oPTYwMzg3NmYxMTQ3MWE0YTU2YTI1NWM5NmQ3MDQxYjJiJm9lPTU4OTg4MEE3IiwiYXVkIjoid2lvYXBwLWM3MGE4IiwiYXV0aF90aW1lIjoxNDgwMzI4NTUwLCJ1c2VyX2lkIjoiT0NibDFoWHBVUmNHcDFzOE12eUR4QTI3UDVQMiIsInN1YiI6Ik9DYmwxaFhwVVJjR3AxczhNdnlEeEEyN1A1UDIiLCJpYXQiOjE0ODE3NjYwNTQsImV4cCI6MTQ4MTc2OTY1NCwiZW1haWwiOiJhd29uZzE5MDBAaG90bWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZmFjZWJvb2suY29tIjpbIjI2ODAzNjEwMzU2MDI2NCJdLCJlbWFpbCI6WyJhd29uZzE5MDBAaG90bWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJmYWNlYm9vay5jb20ifX0.WFWqPYaKVTqn68nA15g2kJ7zTq9mDGmShoHPIz3hq-6FGmqjVPx08K87Eev0x7FXYcTL_u-AT8yc69KYKvAdw8lqC5BdWGRoF5OOzSLyXIK3sVtXM9rQwqccHjrPBUadRp414-wHMNG3KcRJPa3NYJ9tdqKeIkx0kTiT32ZMbEMUvb686TiQmDrp6QthORe7ETj23NX0hrOFldLYR2oV_vOzi0UVMbsNIWHGomHHaVD4i5_UlY08W5K46XSf4Sp1NUzsc3149oQ8J2sSCIUSk5jGIVqj6pltDf0GaGFkjBwp-vc0NHF6HraYyyO59VxI2rpHahCoc9-MOITmXdOgsg'
 
 # set to si for metric, leave blank for imperial
-units = 'si'
+units = ''
 
 # print("25°C")
 # print("---")
@@ -46,7 +46,11 @@ def get_unit():
   
 def main():
     temp = get_temp()
-    print("{}°{}".format(temp['temperature'], get_unit()))
+    unit = get_unit()
+    if unit == 'C':
+        print("{}°{}".format(temp['temperature'], unit))
+    else:
+        print("{}°{}".format(temp['temperature_f'], unit))
     print("---")
     print("{} | size=18 href=https://api.temp-io.life/v1/users/{}/temps/{}?access_token={}".format(
         temp['name'], user_id, temp_id, token))
